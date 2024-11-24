04f8b0b6e0ec65fb5c4f486cdb673720dc2200b7
wtr <2736476727@qq.com>
2024-11-24 18:54:50 +0800

    Redis环境的配置

--- src/main/java/com/itheima/reggie/config/RedisConfig.java
+++ src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -10,7 +10,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
-
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
